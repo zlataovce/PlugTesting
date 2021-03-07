aaec30ea1a9b72ad7703623cac582186fa151010
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-03-07 16:02:38 +0100

    cool stuff

--- libs/jobs.py
+++ libs/jobs.py
@@ -5,6 +5,7 @@ from libs.paper_api import PaperAPI
 from urllib.request import urlretrieve
 from libs.jenkins_api import JenkinsAPI
 from libs.github_api import GitHubAPI
+from termcolor import colored
 
 
 class Deploy:
@@ -13,32 +14,32 @@ class Deploy:
         self.build_number = build_number
         self.job_folder = cfg['jobs_directory'] + "/" + build_number
         self.job_cfg = job_cfg
-        print("Preparing build directories...")
+        print(colored("Preparing build directories...", "cyan"))
         if not exists(cfg['jobs_directory']):
             mkdir(cfg['jobs_directory'])
         if exists(self.job_folder):
-            print("Build already exists! Overriding.")
+            print(colored("Build already exists! Overriding.", "yellow"))
             rmtree(self.job_folder)
         mkdir(self.job_folder)
         mkdir(self.job_folder + "/plugins")
 
     def prepare_server(self):
         api = PaperAPI(self.job_cfg['version'])
-        print("Resolving build info from PaperAPI...")
+        print(colored("Resolving build info from PaperAPI...", "cyan"))
         if self.job_cfg['paper_build'] == "latest":
             url = api.latest()
         else:
             url = api.build(self.job_cfg['paper_build'])
-        print("Downloading the Paper jar... This may take a while.")
+        print(colored("Downloading the Paper JAR... This may take a while.", "cyan"))
         urlretrieve(url, self.job_folder + "/paper.jar")
-        print("Copying files...")
+        print(colored("Copying files...", "cyan"))
         copyfile("libs/eula.txt", self.job_folder + "/eula.txt")
         copyfile(self.job_cfg['properties'], self.job_folder + "/server.properties")
 
     def resolve_deps(self):
-        print("Resolving dependencies...")
+        print(colored("Resolving dependencies...", "cyan"))
         for i in self.job_cfg['dependencies']:
-            print("Getting " + i + "...")
+            print(colored("Getting " + i + "...", "magenta"))
             if self.job_cfg['dependencies'][i]["jenkins"] is not False and self.job_cfg['dependencies'][i]["jenkins"][0] is True:
                 api = JenkinsAPI(self.job_cfg['dependencies'][i]["url"])
                 url = api.latest(self.job_cfg['dependencies'][i]["jenkins"][-1])
@@ -54,9 +55,9 @@ class Deploy:
                 urlretrieve(self.job_cfg['dependencies'][i]["url"], self.job_folder + "/plugins/" + i + ".jar")
 
     def resolve_subjects(self):
-        print("Resolving testing subjects...")
+        print(colored("Resolving testing subjects...", "cyan"))
         for i in self.job_cfg['subject']:
-            print("Getting " + i + "...")
+            print(colored("Getting " + i + "...", "magenta"))
             if self.job_cfg['subject'][i]["jenkins"] is not False and self.job_cfg['subject'][i]["jenkins"][0] is True:
                 api = JenkinsAPI(self.job_cfg['subject'][i]["url"])
                 url = api.latest(self.job_cfg['subject'][i]["jenkins"][-1])
@@ -72,5 +73,5 @@ class Deploy:
                 urlretrieve(self.job_cfg['subject'][i]["url"], self.job_folder + "/plugins/" + i + ".jar")
 
     def finalize(self):
-        print("Packing artifacts...")
-        make_archive("artifact.zip", "zip", self.job_folder)
+        print(colored("Packing artifacts...", "cyan"))
+        make_archive(self.cfg['jobs_directory'] + "/artifact" + self.build_number, "zip", self.job_folder)
